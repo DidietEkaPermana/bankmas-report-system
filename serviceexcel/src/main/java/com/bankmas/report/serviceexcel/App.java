371856db87c5c0712bc43e313a4fcdf4589d9d54
DidietEkaPermana <didiet_eka_permana@hotmail.com>
2024-01-21 16:31:19 +0700

    init project

---
+++ serviceexcel/src/main/java/com/bankmas/report/serviceexcel/App.java
@@ -0,0 +1,13 @@
+package com.bankmas.report.serviceexcel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
